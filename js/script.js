271d3210b316c967215a67d739f4b627a8cfc012
Rusiko <r.osmanov@mail.ru>
2020-02-21 17:07:54 +0300

    1

--- js/script.js
+++ js/script.js
@@ -4,11 +4,21 @@ $(window).scroll(function(){
         $('.header_line_2').addClass('scroll');
         $('.logo-2').addClass('scroll');
         $('.logo-1').addClass('scroll');
+        $('.border_white').addClass('scroll');
+        $('.border_white_2').addClass('scroll');
+        $('.add_order').addClass('scroll');
+        $('.will_contractor').addClass('scroll');
+
     }
     else {
-        $('.header_line').removeClass('scroll')
-        $('.header_line_2').removeClass('scroll')
-        $('.logo-2').removeClass('scroll')
-        $('.logo-1').removeClass('scroll')
+        $('.header_line').removeClass('scroll');
+        $('.header_line_2').removeClass('scroll');
+        $('.logo-2').removeClass('scroll');
+        $('.logo-1').removeClass('scroll');
+        $('.border_white').removeClass('scroll');
+        $('.border_white_2').removeClass('scroll');
+        $('.add_order').removeClass('scroll');
+        $('.will_contractor').removeClass('scroll');
+
     }
 });
